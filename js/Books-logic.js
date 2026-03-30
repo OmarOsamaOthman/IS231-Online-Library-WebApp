4ef2dc8001a66732ce27d5df1079a325b65575a6
Zyad <zezozoz131@gmail.com>
2026-03-30 07:08:27 +0200

    update books

--- js/Books-logic.js
+++ js/Books-logic.js
@@ -2,32 +2,48 @@ const trendingSection = document.getElementById("main-books");
 const codingSection = document.getElementById("Coding-books");
 const novelsSection = document.getElementById("Novels");
 
-fetch("../js/books.json")
-  .then((response) => response.json())
-  .then((books) => {
-    books.forEach((book) => {
-      const bookCard = document.createElement("div");
-      bookCard.className = "book";
-      bookCard.innerHTML = `
-        <h2>${book.title}</h2>
+let borrowed = JSON.parse(localStorage.getItem("Borrowed-Books")) || [];
+
+let storedBooks = JSON.parse(localStorage.getItem("allBooks"));
+
+if (storedBooks) {
+  renderBooks(storedBooks);
+} else {
+  fetch("../js/books.json")
+    .then((response) => response.json())
+    .then((books) => {
+      localStorage.setItem("allBooks", JSON.stringify(books));
+      renderBooks(books);
+    });
+}
+
+function renderBooks(books) {
+  localStorage.setItem("allBooks", JSON.stringify(books));
+  let count = 0;
+  books.forEach((book) => {
+    count++;
+    const bookCard = document.createElement("div");
+    bookCard.className = "book";
+    bookCard.innerHTML = `
+      <h2>${book.title}</h2>
         <a href="#" data-id="${book.id}">
           <img src="${book.image}" alt="${book.title}" />
         </a>
         <p>by ${book.author}</p>
-        <button>Borrow</button>
+        <button id= "${count}" onclick = "handleBtnBorrow(${count})">Borrow</button>
       `;
 
-      if (book.category === "Trending") {
-        trendingSection.appendChild(bookCard);
-      } else if (book.category === "Coding") {
-        codingSection.appendChild(bookCard);
-      } else if (book.category === "Novels") {
-        novelsSection.appendChild(bookCard);
-      }
-    });
-
-    addBookClickEvents(books);
+    if (book.category === "Trending") {
+      trendingSection.appendChild(bookCard);
+    } else if (book.category === "Coding") {
+      codingSection.appendChild(bookCard);
+    } else if (book.category === "Novels") {
+      novelsSection.appendChild(bookCard);
+    }
   });
+
+  addBookClickEvents(books);
+}
 
 function addBookClickEvents(books) {
   const bookAnchors = document.querySelectorAll(".book a");
@@ -42,6 +58,46 @@ function addBookClickEvents(books) {
       window.location.href = "./book-detail.html";
     });
   });
+}
+
+function handleBtnBorrow(id) {
+  const booksList = JSON.parse(localStorage.getItem("allBooks"));
+  for (let i = 0; i < booksList.length; i++) {
+    if (id === booksList[i].id) {
+      console.log("YES - found book:", booksList[i]);
+      if (booksList[i].status === "available") {
+        borrowed.push(booksList[i]);
+        booksList[i].status = "borrowed";
+        localStorage.setItem("allBooks", JSON.stringify(booksList));
+
+        localStorage.setItem("Borrowed-Books", JSON.stringify(borrowed));
+        console.log(`Book avilabile`);
+        showCongrats();
+      } else {
+        warning();
+        console.log(`Book Unavilabile`);
+      }
+      break;
+    }
+  }
+}
+
+function showCongrats() {
+  const congrats = document.getElementById("congrats");
+  congrats.style.display = "block";
+
+  setTimeout(() => {
+    congrats.style.display = "none";
+  }, 3000);
+}
+
+function warning() {
+  const warn = document.getElementById("warning");
+  warn.style.display = "block";
+
+  setTimeout(() => {
+    warn.style.display = "none";
+  }, 3000);
 }
 
 // const myAnchors = document.querySelectorAll("a");
