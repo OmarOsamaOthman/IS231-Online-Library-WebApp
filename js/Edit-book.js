743b771cf375cd58f985a41f0ccd553a2382916b
Omar Osama <20240843@stud.fci-cu.edu.eg>
2026-03-31 08:51:06 +0200

    edit book logic

--- js/Edit-book.js
+++ js/Edit-book.js
@@ -1,22 +1,16 @@
-let liveBook = document.createElement("div");
+let editBook = JSON.parse(localStorage.getItem("selectedBookToEdit")) || [];
 
-liveBook.innerHTML = `
-        <div class="details-card">
-            <img id="image-book" src="" alt="" />
-            <div class="book-info">
-                <h2 id="title-book"></h2>
-                <p><strong>Author:</strong> <span id="author-book"></span></p>
-                <p><strong>Category:</strong> <span id="category-book"></span></p>
-                <p><strong>Status:</strong> <span id="status-book"></span></p>
-                <p>
-                    <strong>Description:</strong> <span id="description-book"></span>
-                </p>
-            </div>
-        </div>
-`;
+let allBooks = JSON.parse(localStorage.getItem("allBooks")) || [];
+
+let preview = document.getElementsByClassName("preview")[0];
+
+const formSub = document.getElementById("form");
 
 
 
+// preview elements section
+
+// create containers
 let card = document.createElement("div");
 let img = document.createElement("img");
 let title = document.createElement("h2");
@@ -25,39 +19,206 @@ let category = document.createElement("span");
 let status = document.createElement("span");
 let description = document.createElement("span");
 
+// create texts
+let titleText = document.createTextNode(editBook.title);
+let authorText = document.createTextNode(editBook.author);
+let categoryText = document.createTextNode(editBook.category);
+let statusText = document.createTextNode(editBook.status);
+let descriptionText = document.createTextNode(editBook.description);
+let cover = document.createElement("img");
 
-let titleText = document.createTextNode("Python Book");
-// let authorText = document.createTextNode();
-// let categoryText = document.createTextNode();
-// let statusText = document.createTextNode();
 
+
+// set values
 title.appendChild(titleText);
+title.setAttribute("id", "book-title");
+
+author.appendChild(authorText);
+author.setAttribute("id", "book-author");
+
+category.appendChild(categoryText);
+category.setAttribute("id", "book-category");
+
+status.appendChild(statusText);
+status.setAttribute("id", "book-status");
+
+description.appendChild(descriptionText);
+description.setAttribute("id", "book-description");
+
+cover.setAttribute("src", editBook.image);
+cover.setAttribute("alt", "Book-cover");
+
+card.innerHTML = `
+            <div class="cover"></div>
+            <div class="info">
+                <div class="header"></div>
+                <div class="author-cont">
+                    <p>Author:</p>
+                </div>
+                <div class="category-cont">
+                    <p>Category:</p>
+                </div>
+                <div class="status-cont">
+                    <p>Status:</p>
+                </div>
+                <div class="description-cont">
+                    <p>Description:</p>
+                </div>
+            </div>
+`
+
+// appending the elements
+preview.appendChild(card);
+card.setAttribute("class", "card-div");
+
+card.querySelector(".header").appendChild(title);
+card.querySelector(".cover").appendChild(cover);
+
+card.querySelector(".author-cont").firstElementChild.appendChild(author);
+card.querySelector(".category-cont").firstElementChild.appendChild(category);
+card.querySelector(".status-cont").firstElementChild.appendChild(status);
+card.querySelector(".description-cont").firstElementChild.appendChild(description);
 
 
-// let title = document.createElement("h2");
-// title = liveBook.querySelector("#title-book");
-// // title = liveBook.querySelector("#title-book");
-// let author = liveBook.querySelector("#author-book");
-// let category = liveBook.querySelector("#category-book");
-// let stat = liveBook.querySelector("#status-book")
-// let desc = liveBook.querySelector("#description-book");
 
 
-// title.innerText = "Python Book";
-// let bookName = title.innerText;
-
-// console.log(img);
-// console.log(bookName);
-// console.log(author);
-// console.log(category);
-// console.log(stat);
-// console.log(desc);
 
 
-let preview = document.getElementsByClassName("preview")[0];
 
-console.log(preview);
+// editing section
 
-preview.appendChild(title);
+// create elements
+let titleInput = document.querySelector("main form #book-title");
+let authorInput = document.querySelector("main form #author");
+let categoryInput = document.querySelector("main form #category");
+let descriptionInput = document.querySelector("main form #description");
+let statusInput = document.querySelector("main form #status");
+let imageInput = document.querySelector("main form #book-image");
 
 
+
+titleInput.setAttribute("value", titleText.nodeValue);
+authorInput.setAttribute("value", authorText.nodeValue);
+
+
+for (let i = 0; i < categoryInput.length; i++) {
+    if (categoryInput.children[i].innerHTML == categoryText.nodeValue) {
+        categoryInput.children[i].setAttribute("selected", "");
+        break;
+    }
+}
+
+descriptionInput.innerHTML = editBook.description;
+
+for (let i = 0; i < statusInput.length; i++) {
+    statusText.nodeValue = statusText.nodeValue[0].toUpperCase() + statusText.textContent.slice(1);
+    if (statusInput.children[i].innerHTML == statusText.nodeValue) {
+        statusInput.children[i].setAttribute("selected", "");
+        break;
+    }
+}
+
+
+
+
+titleInput.addEventListener("input", function () {
+    title.textContent = titleInput.value.trim() || "Book Title";
+});
+
+authorInput.addEventListener("input", function () {
+    author.textContent = authorInput.value.trim() || "Book Author";
+
+});
+
+
+categoryInput.addEventListener("input", function () {
+    category.textContent = categoryInput.value.trim() || "Book category";
+
+});
+
+descriptionInput.addEventListener("input", function () {
+    description.textContent = descriptionInput.value.trim() || "Book category";
+
+});
+
+statusInput.addEventListener("input", function () {
+    status.textContent = statusInput.value.trim() || "Book category";
+});
+
+
+imageInput.addEventListener("change", function () {
+
+    let file = imageInput.files[0];
+
+    if (!file) return;
+
+    cover.src = URL.createObjectURL(file);
+
+    cover.setAttribute("alt", editBook.title);
+});
+
+
+
+
+console.log(allBooks);
+
+formSub.onsubmit = function (p) {
+
+    let titleValue = document.getElementById("book-title").value;
+    let authorValue = document.getElementById("author").value;
+    let categoryValue = document.getElementById("category").value;
+    let descriptionValue = document.getElementById("description").value;
+    let statusValue = document.getElementById("status").value;
+
+    let file = document.getElementById("book-image").files[0];
+
+    p.preventDefault();
+
+
+    console.log(titleValue);
+    console.log(authorValue);
+
+    editBook.title = titleValue;
+    editBook.author = authorValue;
+    editBook.category = categoryValue;
+    editBook.description = descriptionValue;
+    editBook.status = statusValue;
+
+
+    // if (!file) {
+    //     alert("Please choose an image");
+    //     return;
+    // }
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const imageBase64 = e.target.result;
+
+        console.log(imageBase64.src);
+
+
+        for (let i = 0; i < allBooks.length; i++) {
+
+            console.log(editBook);
+            if (allBooks[i].id == editBook.id) {
+
+                allBooks[i].title = titleValue;
+                allBooks[i].author = authorValue;
+                allBooks[i].category = categoryValue;
+                allBooks[i].description = descriptionValue;
+                allBooks[i].status = statusValue;
+                allBooks[i].id = editBook.id;
+                allBooks[i].image = imageBase64;
+            }
+
+            console.log(allBooks[i]);
+        }
+
+        localStorage.setItem("allBooks", JSON.stringify(allBooks));
+
+    }
+
+    reader.readAsDataURL(file);
+    window.location.href = ("../html/books.html");
+};
