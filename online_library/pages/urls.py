8043f215a7f494901d7af7d3c48aca42ff82769d
Omar Osama <20240843@stud.fci-cu.edu.eg>
2026-05-07 21:49:27 +0300

    convert frontend to static in django

---
+++ online_library/pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path 
+from . import views
+
+urlpatterns=[
+    path('index/',views.index ,name='index'),
+    path('about/' ,views.about ,name='about'),
+    path('login/' ,views.login_view ,name='login_view'),
+    path('signup/' ,views.signup ,name='signup'),
+    path('library/' ,views.library ,name='library'),
+    path('add_book/' ,views.add_book ,name='add_book'),
+    path('edit_book/' ,views.edit_book ,name='edit_book'),
+    path('books/' ,views.books ,name='books'),
+    
+]
